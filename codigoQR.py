aa82ca5e3cac59a656604d53a5c927140f3f9e53
Orlando Ulises Aguilar Rojas <orlandoulisesit@gmail.com>
2022-11-05 12:34:44 -0600

    Bot Final

--- codigoQR.py
+++ codigoQR.py
@@ -1,23 +1,25 @@
 import qrcode
 from os import remove
 
+
 def crearImagenQR(textoQR, nombreArchivo):
-    #Se crea el codigo qr
+    # Se crea el codigo qr
     try:
         img = qrcode.make(textoQR)
         img.save("{}.png".format(nombreArchivo))
         return "{}.png".format(nombreArchivo)
-        
+
     except:
         pass
 
+
 def enviarCodigoQR(textoQR, nombreArchivo, usuarioid, bot):
     PHOTO_PATH = crearImagenQR(textoQR, nombreArchivo)
-    
+
     try:
-        bot.send_photo(chat_id = usuarioid, photo=open(PHOTO_PATH, 'rb'))
-        
+        bot.send_photo(chat_id=usuarioid, photo=open(PHOTO_PATH, 'rb'))
+
     except:
         pass
-    
+
     remove(PHOTO_PATH)
